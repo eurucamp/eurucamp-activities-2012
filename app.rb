f927db07ac5f76e854bdd5040d2ab87323f7ebab
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-12 11:11:40 +0200

    additional methods in Status

--- app.rb
+++ app.rb
@@ -4,6 +4,24 @@ require "sinatra"
 require "haml"
 require "mongoid"
 
+Twitter::Status.class_eval do
+  def in?
+    @_in ||= !(text !~ /@#{Settings.twitter.account}\W+#{Settings.twitter.tokens.im_in}\W*/)
+  end
+
+  def out?
+    @_out ||= !(text !~ /@#{Settings.twitter.account}\W+#{Settings.twitter.tokens.im_out}\W*/)
+  end
+
+  def code
+    text.gsub(/@#{Settings.twitter.account}\W+#{Settings.twitter.tokens.send(in? ? :im_in : :im_out)}\W*/,"") if in? || out?
+  end
+
+  def attendee
+    from_user
+  end
+end
+
 module Eurucamp
   module Activities
 
@@ -19,10 +37,6 @@ module Eurucamp
 
     class Job
       def run
-        #Twitter.search("to:#{Settings.twitter.account} '#{Settings.twitter.tokens.im_in}'", :result_type => "recent").results.map do |status|
-        #  most_likely_a_code = status.text.gsub(/@#{Settings.twitter.account}\W+#{Settings.twitter.tokens.im_in}\W*/,"")
-        #  p most_likely_a_code
-        #end
       end
     end
 
