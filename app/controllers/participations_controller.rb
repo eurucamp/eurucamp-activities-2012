142dc7ebfa569b104e748f6baa8f78a243c4c2d5
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-13 19:06:44 +0200

    fetch statuses in :dev

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,6 +2,7 @@ class ParticipationsController < ApplicationController
   respond_to :html
 
   def index
+    FetchStatusesJob.new.run! if Rails.env.development?
     @activities = Repository.all
     respond_with @activities
   end
