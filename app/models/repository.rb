c82ff7e4c38a1877aab8397172813a83942b2f75
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-13 23:01:35 +0200

    dashboard fixed

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,15 +1,8 @@
 class Repository
-  def self.activities
-    Settings.activities.sort { |x,y| x["when"] <=> y["when"] }
-  end
-
-  def self.codes
-    activities.map { |a| a["code"] }
-  end
-
   def self.all
-    activities.map do |activity|
-      activity.merge("participations" => Participation.where(:code => activity["code"]))
-    end
+    Activity.all
+    #activities.map do |activity|
+    #  activity.merge("participations" => Participation.where(:code => activity["code"]))
+    #end
   end
 end
