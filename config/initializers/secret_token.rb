ffe5bcceb5a94cac8ec481193f23db203d407de9
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-13 17:56:53 +0200

    added basic rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EurucampActivities::Application.config.secret_token = '4ce9e1cdcf0783ba0fb37897a3e2411165c398d3a33e0d107cd51a0c502c83e4157e995b573bc6d45cf9a97ceceb6a159bacf2175c5b45b50f6fc4a6e7cf15c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EurucampActivities::Application.config.session_store :cookie_store, key: '_eurucamp-activities_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EurucampActivities::Application.config.session_store :active_record_store
