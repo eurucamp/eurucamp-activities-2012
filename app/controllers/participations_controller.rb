b29efe7f87de178a1529440ef60da08c1fc135ae
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-13 21:35:52 +0200

    Revert "added logger"
    This reverts commit 326b17767e3237d958e54e009ccb322b6a7be468.

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,7 +2,6 @@ class ParticipationsController < ApplicationController
   respond_to :html
 
   def index
-    Rails.logger.info "@@@@@@#{Rails.env} ENV: #{ENV.inspect}"
     FetchStatusesJob.new.run! if Rails.env.development?
     @activities = Repository.all
     respond_with @activities
