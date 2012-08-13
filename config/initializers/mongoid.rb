6354594b3c8e39a3d1f3fdf77d2a39dc9fac07e3
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-13 19:30:08 +0200

    read the config

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,6 +1,6 @@
 Mongoid.logger.level = Logger::DEBUG
 Moped.logger.level = Logger::DEBUG
 
-# Mongoid.load!("./config/mongoid.yml")
+Mongoid.load!("./config/mongoid.yml")
 Participation.create_indexes
 User.create_indexes
