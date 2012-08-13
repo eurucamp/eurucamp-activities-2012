326b17767e3237d958e54e009ccb322b6a7be468
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-13 21:07:36 +0200

    added logger

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,6 +2,7 @@ class ParticipationsController < ApplicationController
   respond_to :html
 
   def index
+    Rails.logger.info "@@@@@@#{Rails.env} ENV: #{ENV.inspect}"
     FetchStatusesJob.new.run! if Rails.env.development?
     @activities = Repository.all
     respond_with @activities
