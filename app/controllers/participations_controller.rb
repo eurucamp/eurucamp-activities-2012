8a8ac216119a3ffdcef880146ab51402bee5dc29
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-13 22:53:56 +0200

    don't run it for now

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,7 +2,7 @@ class ParticipationsController < ApplicationController
   respond_to :html
 
   def index
-    FetchStatusesJob.new.run! if Rails.env.development?
+    # FetchStatusesJob.new.run! if Rails.env.development?
     @activities = Repository.all
     respond_with @activities
   end
