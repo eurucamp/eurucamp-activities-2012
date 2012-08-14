915d7c448d439d2c0948dd778f934140cc2b9152
Polarblau <polarblau@gmail.com>
2012-08-14 18:41:38 +0300

    Header, footer and helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+
+   def wrapped(el = :div, attrs = {}, &block)
+    capture_haml do
+      haml_tag el, attrs do
+        haml_tag :div, :class => 'wrapper', &block
+      end
+    end
+  end
+
+  def image(url, sizes = [], attrs = {})
+    attrs[:src] = url
+    attrs = inject_class(attrs, 'resp')
+    sizes.each do |size|
+      attrs[:"data-#{size}"] = image_url_for_size(url, size)
+    end
+    capture_haml do
+      haml_tag :img, attrs
+    end
+  end
+
+private
+
+  def inject_class(attributes, klass)
+    attrs = attributes.dup
+    attrs[:class] ||= ''
+    attrs[:class] << " #{klass}"
+    attrs[:class].strip!
+    attrs
+  end
+
+  def image_url_for_size(url, size)
+    extension = File.extname(url)
+    url.chomp(extension) + '-' + size.to_s + extension
+  end
 end
