a3e97b588561032e98ead2b1eaa8f33b2ed31bac
Polarblau <polarblau@gmail.com>
2012-08-15 16:52:47 +0300

    Basic layout.

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,7 +2,7 @@ class ParticipationsController < ApplicationController
   respond_to :html
 
   def index
-    FetchStatusesJob.new.run! if Rails.env.development?
+    # FetchStatusesJob.new.run! if Rails.env.development?
     @activities = Activity.published
     respond_with @activities
   end
