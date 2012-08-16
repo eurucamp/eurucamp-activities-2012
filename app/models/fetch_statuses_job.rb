a3a2850831c47bc15363202bff6b04fcc7e4203e
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-08-16 14:14:03 +0200

    use SortedSet

--- app/models/fetch_statuses_job.rb
+++ app/models/fetch_statuses_job.rb
@@ -1,7 +1,8 @@
 class FetchStatusesJob
   def run!
     query = "to:#{Settings.twitter.account} '#{Settings.twitter.tokens.im_in} OR #{Settings.twitter.tokens.im_out}'"
-    Twitter.search(query, :rpp => 100, :result_type => "recent").results.sort{ |x, y| x.created_at <=> y.created_at }.map do |status|
+
+    SortedSet(Twitter.search(query, :rpp => 100, :result_type => "recent").results).map do |status|
       activity = Activity.where(:code => status.code).first
       if activity
         args = {:code => status.code, :account => status.from_user}
