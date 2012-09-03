e9ecca7909f7eba52b488b45e97c298e361e422d
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-09-03 13:02:51 +0200

    cleanups in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :trackable, :validatable
 
   ## Database authenticatable
@@ -25,20 +22,6 @@ class User
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
 
-  ## Confirmable
-  # field :confirmation_token,   :type => String
-  # field :confirmed_at,         :type => Time
-  # field :confirmation_sent_at, :type => Time
-  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
-  # field :locked_at,       :type => Time
-
-  ## Token authenticatable
-  # field :authentication_token, :type => String
-  # run 'rake db:mongoid:create_indexes' to create indexes
   index({ email: 1 }, { unique: true, background: true })
   field :name, :type => String
   validates_presence_of :name
